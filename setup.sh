78241b5e8ffb021a6289ddf1956e0dfdd40f4d41
artem-v-shamsutdinov <Artem.V.Shamsutdinov@dataindependence.net>
2024-09-11 21:02:35 -0600

    Script fix.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+cd ..
+
 git clone https://github.com/artem-v-shamsutdinov/may-there-always-be-sunshine.space.git
 git clone https://github.com/artem-v-shamsutdinov/past-the-war.earth.git
 git clone https://github.com/Data-Independence-Network/dataindependence.net.git
 git clone https://github.com/beyond-decentralized/beyond-decentralized.world.git
 git clone https://github.com/artem-v-shamsutdinov/turbase.org.git
 git clone https://github.com/artem-v-shamsutdinov/notes.git
-git clone https://github.com/artem-v-shamsutdinov/what-can.be.git
 
 ln -s ./may-there-always-be-sunshine.space/dist/public ./what-can.be/dist/public/space
 ln -s ./past-the-war.earth/dist/public ./what-can.be/dist/public/peace
