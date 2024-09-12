2084555352776186effc18aaf9a1613eefe7d98c
artem-v-shamsutdinov <Artem.V.Shamsutdinov@dataindependence.net>
2024-09-11 21:27:13 -0600

    Script fix.

--- setup.sh
+++ setup.sh
@@ -9,9 +9,9 @@ git clone https://github.com/beyond-decentralized/beyond-decentralized.world.git
 git clone https://github.com/artem-v-shamsutdinov/turbase.org.git
 git clone https://github.com/artem-v-shamsutdinov/notes.git
 
-ln -s ./may-there-always-be-sunshine.space/dist/public ./what-can.be/dist/public/space
-ln -s ./past-the-war.earth/dist/public ./what-can.be/dist/public/peace
-ln -s ./dataindependence/dist/public ./what-can.be/dist/public/dataindependence
-ln -s ./beyond-decentralized/dist/public ./what-can.be/dist/public/beyond-decentralized
-ln -s ./turbase.org/dist/public ./what-can.be/dist/public/turbase
-ln -s ./notes ./what-can.be/dist/public/space/notes
+ln -s ~/may-there-always-be-sunshine.space/dist/public ./what-can.be/dist/public/space
+ln -s ~/past-the-war.earth/dist/public ./what-can.be/dist/public/peace
+ln -s ~/dataindependence.net/dist/public ./what-can.be/dist/public/dataindependence
+ln -s ~/beyond-decentralized.world/dist/public ./what-can.be/dist/public/beyond-decentralized
+ln -s ~/turbase.org/dist/public ./what-can.be/dist/public/turbase
+ln -s ~/notes ~/may-there-always-be-sunshine.space/dist/public/notes
